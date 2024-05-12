78ea14c62d6bbefee09ef7698e8fd87d13668fe5
Shurik <156313194+ShurikChan@users.noreply.github.com>
2024-05-12 11:48:34 +0300

    Update settings.py

--- NewsPortal/settings.py
+++ NewsPortal/settings.py
@@ -162,8 +162,8 @@ ACCOUNT_FORMS = {'signup': 'sign.models.BasicSignupForm'}
 
 EMAIL_HOST = 'smtp.yandex.ru'  # адрес сервера Яндекс-почты для всех один и тот же
 EMAIL_PORT = 465  # порт smtp сервера тоже одинаковый
-EMAIL_HOST_USER = 'shurikchann@yandex.ru'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
-EMAIL_HOST_PASSWORD = 'ryqdmsmzxfzhiuqr'  # пароль от почты
+EMAIL_HOST_USER = '-'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
+EMAIL_HOST_PASSWORD = '-'  # пароль от почты
 EMAIL_USE_SSL = True
 
 
